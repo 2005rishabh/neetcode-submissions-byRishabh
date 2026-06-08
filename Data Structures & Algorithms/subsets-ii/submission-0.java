cf060cd69c7d2c47bd5dc7d308e383c2fcb1b79f
2005rishabh <161556896+2005rishabh@users.noreply.github.com>
2026-06-08 11:33:06 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums); 
+        HashSet<List<Integer>> ll = new HashSet<>();
+        fun(nums, 0, ll, new ArrayList<>());
+        return new ArrayList<>(ll);
+    }
+
+    public void fun(int[] nums, int i, HashSet<List<Integer>> result, List<Integer> currentSubset) {
+        if (i == nums.length) {
+            result.add(new ArrayList<>(currentSubset));
+            return;
+        }
+
+        fun(nums, i + 1, result, currentSubset);
+
+        currentSubset.add(nums[i]);
+        fun(nums, i + 1, result, currentSubset);
+
+        currentSubset.remove(currentSubset.size() - 1);
+    }
+}
