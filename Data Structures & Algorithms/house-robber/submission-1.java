4788c3903622752de5463340bba4951f0b3617cf
2005rishabh <161556896+2005rishabh@users.noreply.github.com>
2026-06-14 11:15:02 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] cost) {
+        int[] dp =  new int[cost.length];
+        Arrays.fill(dp, -1);
+        return Math.max(dfs(0, cost, dp), dfs(1, cost, dp));
+        // int n = cost.length;
+        // int[] dp = new int[n + 1];
+
+        // for (int i = 2; i <= n; i++) {
+        //     dp[i] = Math.min(dp[i - 1] + cost[i - 1],
+        //                      dp[i - 2] + cost[i - 2]);
+        // }
+
+        // return dp[n];
+    }
+
+
+    int dfs(int i, int[] c, int[] dp) {
+        if(i >= c.length) return 0;
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = c[i] + Math.max(dfs(i+2, c, dp), dfs(i+3, c, dp));
+    }   
+}
