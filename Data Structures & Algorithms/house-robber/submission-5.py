b485e598e6203b4c3e649bd42e09e93402ab092c
2005rishabh <161556896+2005rishabh@users.noreply.github.com>
2026-08-18 11:59:55 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, cost: List[int]) -> int:
+        dp = [-1] * len(cost)
+
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            if dp[i] != -1:
+                return dp[i]
+
+            dp[i] =max(dfs(i + 1), cost[i] + dfs(i+ 2))
+
+            return dp[i]; 
+
+        return dfs(0)
+        
+        
+        # cost.append(0)
+        # for i in range(len(cost) - 3, -1, -1):
+        #     cost[i] += min(cost[i + 1] , cost[i + 2])
+        
+        # return min(cost[0], cost[1])
